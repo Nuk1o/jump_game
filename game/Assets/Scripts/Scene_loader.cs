d2ef7f580502b2a1786b5858693773f3b688797b
Nukio <NukiXm@yandex.ru>
2023-01-21 01:06:10 +0300

    Create menu game

--- game/Assets/Scripts/Scene_loader.cs
+++ game/Assets/Scripts/Scene_loader.cs
@@ -3,9 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class Scene_loader : MonoBehaviour
 {
+    public void load_s(string name_s)
+    {
+        SceneManager.LoadScene(name_s);
+    }
     public void ReloadScene()
     {
         string currentSceneName = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(currentSceneName);
+    }
+    public void exit_app()
+    {
+        Application.Quit();
     }
 }
